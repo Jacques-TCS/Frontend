dbf6b2c9acfdff771b999a224f611eee18e47af7
betossauro <pfvrbeto@gmail.com>
2024-03-04 06:27:16 -0300

    update/aplicandoTailwindFront

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { createMask } from '@ngneat/input-mask';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'SistemaRegistroVerificacao';
+  isDarkTheme: boolean;
+
+  ngOnInit(): void {
+    initFlowbite();
+    this.isDarkTheme = localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches);
+    this.updateTheme();
+  }
+
+  toggleTheme(): void {
+    this.isDarkTheme = !this.isDarkTheme;
+    localStorage.setItem('color-theme', this.isDarkTheme ? 'dark' : 'light');
+    this.updateTheme();
+  }
+
+  private updateTheme(): void {
+    if (this.isDarkTheme) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }
 }
