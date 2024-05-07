9cde099ecd7fc1cf7024bcc91d7f2ff53f06f05e
Rafael Derner <rafael-derner@live.com>
2024-05-07 20:47:40 -0300

    feat/autenticacao Autenticação implementada no frontend. Token sendo salvo no localStorage, e rotas sendo protegidas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { privateRouteGuard } from './servicos/guards/private-route.guard';
+import { publicRouteGuard } from './servicos/guards/public-route.guard';
 import { UsuarioCadastroComponent } from './usuarios/usuario-cadastro/usuario-cadastro.component';
-import { publicRouteGuard } from './shared/service/guards/public-route.guard';
-import { privateRouteGuard } from './shared/service/guards/private-route.guard';
 
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
+    redirectTo: 'menu',
     redirectTo: 'menu',
   },
   {
@@ -15,11 +16,13 @@ const routes: Routes = [
     loadChildren: () =>
       import('./login/login.module').then((m) => m.LoginModule),
     canActivate: [publicRouteGuard]
+    canActivate: [publicRouteGuard]
   },
   {
     path: 'menu',
     loadChildren: () =>
       import('./menu/menu.module').then((m) => m.MenuModule),
+    canActivate: [privateRouteGuard]
     canActivate: [privateRouteGuard]
   },
 ];
