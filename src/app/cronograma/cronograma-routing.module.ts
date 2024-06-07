2dcb9c9a8c0c2dfb0f7b881df038068360b1dc20
barbaraLuersen <barbaraluersenlima@gmail.com>
2024-06-06 21:35:13 -0300

    update/Rota para tela de cronograma

--- src/app/cronograma/cronograma-routing.module.ts
+++ src/app/cronograma/cronograma-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CronogramaCadastroComponent } from './cronograma-cadastro/cronograma-cadastro.component';
+import { CronogramaListagemComponent } from './cronograma-listagem/cronograma-listagem.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: CronogramaCadastroComponent },
+  { path: 'listagem', component: CronogramaListagemComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
