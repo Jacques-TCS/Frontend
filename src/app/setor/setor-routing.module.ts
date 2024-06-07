8b7c06b653da5421fb16eceef37abf7bf6719352
barbaraLuersen <barbaraluersenlima@gmail.com>
2024-06-06 21:34:27 -0300

    update/Rota para tela de setor

--- src/app/setor/setor-routing.module.ts
+++ src/app/setor/setor-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SetorCadastroComponent } from './setor-cadastro/setor-cadastro.component';
+import { SetorListagemComponent } from './setor-listagem/setor-listagem.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SetorCadastroComponent },
+  { path: 'listagem', component: SetorListagemComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
