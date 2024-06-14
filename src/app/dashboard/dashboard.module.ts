2d4fb9024d0b9230177933e2afd2aac0c8578f46
betossauro <pfvrbeto@gmail.com>
2024-06-13 21:44:57 -0300

    update/ajustes de imports

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { HomeComponent } from './home/home.component';
+import { NgApexchartsModule } from 'ng-apexcharts';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, DashboardRoutingModule],
+  imports: [CommonModule, DashboardRoutingModule, NgApexchartsModule],
 })
 export class DashboardModule {}
