63e4b7ab6dd1b7e63b078f8447844d1fd23051c6
beto <betoweberr@gmail.com>
2024-06-22 11:19:02 -0300

    feat/tela de redefinição de senha

--- src/app/usuarios/usuario-perfil/usuario-perfil.component.ts
+++ src/app/usuarios/usuario-perfil/usuario-perfil.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+import { UsuarioService } from 'src/app/shared/service/usuario.service';
+import { Usuario } from 'src/app/shared/model/usuario';
 
 @Component({
   selector: 'app-usuario-perfil',
@@ -6,5 +11,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./usuario-perfil.component.scss']
 })
 export class UsuarioPerfilComponent {
+  @ViewChild('ngForm') public ngForm: NgForm;
 
+  public password: string;
+  public confirmPassword: string;
+  public showPassword: boolean = false;
+  public showConfirmPassword: boolean = false;
+  public error: boolean = false;
+  public usuario: Usuario;
+
+  constructor(
+    private router: Router,
+    private usuarioService: UsuarioService
+  ) {}
+
+  togglePasswordVisibility(): void {
+    this.showPassword = !this.showPassword;
+  }
+
+  toggleConfirmPasswordVisibility(): void {
+    this.showConfirmPassword = !this.showConfirmPassword;
+  }
+
+  redefinirSenha(ngForm: NgForm): void {
+    if (!ngForm.invalid) {
+      if (this.password !== this.confirmPassword) {
+        return;
+      }
+
+      this.usuario.password = this.password;
+      this.usuarioService.redefinirSenha(this.usuario).subscribe({
+        next: data => {
+          Swal.fire('Sucesso', 'Sua senha foi redefinida com sucesso!', 'success')
+            .then(() => {
+              this.router.navigate(['/login']);
+            });
+        },
+        error: err => {
+          Swal.fire('Erro', 'Ocorreu algum erro ao redefinir a senha. Tente novamente mais tarde.', 'error');
+        }
+      });
+    }
+  }
 }
